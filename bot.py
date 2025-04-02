19cc3e0541ba5517886406d117b46072c064c2ec
goreforced <125352619+goreforced@users.noreply.github.com>
2025-04-02 16:28:38 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,15 +5,15 @@ TOKEN = "7977806496:AAHdtcgzJ5mx3sVSaGNSKL-EU9rzjEmmsrI"
 TELEGRAM_URL = f"https://api.telegram.org/bot{TOKEN}/" 
 @app.route('/webhook', methods=['POST']) 
 def webhook(): update = request.get_json() 
-  chat_id = update['message']['chat']['id'] 
-  text = update['message']['text'] 
+    chat_id = update['message']['chat']['id'] 
+    text = update['message']['text'] 
 
-  send_message(chat_id, "Привет!") 
-  return "OK", 200 
+    send_message(chat_id, "Привет!") 
+    return "OK", 200 
 def send_message(chat_id, text): 
-  payload = { "chat_id": chat_id, 
-             "text": text 
-            }
+    payload = { "chat_id": chat_id, 
+               "text": text 
+    }
   requests.post(f"{TELEGRAM_URL}sendMessage", json=payload) 
   if __name__ == "__main__": 
     app.run(host="0.0.0.0", port=5000)
